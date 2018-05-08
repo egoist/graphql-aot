1ddf87978548dba2baf9d390d42aa948f16b5d00
EGOIST <0x142857@gmail.com>
2018-05-08 12:21:53 +0800

    allow to use schema directly

---
+++ test/fixtures/use-schema/index.js
@@ -0,0 +1,1 @@
+import foo from './query.gql'
--- test/loader.test.js
+++ test/loader.test.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const { promisify } = require('util')
 const webpack = require('webpack')
+const { makeExecutableSchema } = require('graphql-tools')
 
 const loader = require.resolve('../lib/loader')
 
@@ -23,6 +24,56 @@ test('main', async () => {
           options: {
             defaultClientOptions: {
               uri: 'https://api.graph.cool/simple/v1/cjc32dadu12pf0184otnrztnq'
+            }
+          }
+        }
+      ]
+    }
+  })
+  compiler.outputFileSystem = mfs
+
+  const stats = await promisify(compiler.run.bind(compiler))()
+
+  if (stats.hasErrors()) {
+    throw new Error(stats.toString('errors-only'))
+  }
+
+  expect(mfs.readFileSync('/tmp/main.js', 'utf8')).toMatchSnapshot()
+})
+
+test('use schema', async () => {
+  const MFS = webpack.MemoryOutputFileSystem
+  const mfs = new MFS()
+  const compiler = webpack({
+    entry: path.join(__dirname, 'fixtures/use-schema', 'index.js'),
+    mode: 'development',
+    devtool: false,
+    output: {
+      path: '/tmp/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.gql$/,
+          type: 'json',
+          loader,
+          options: {
+            useSchema: {
+              schema: makeExecutableSchema({
+                typeDefs: `type Query { hello(name: String): String }`,
+                resolvers: {
+                  Query: {
+                    hello: (root, args, context) => {
+                      return `hello ${args.name} ${context.type}`
+                    }
+                  }
+                }
+              }),
+              getContext() {
+                return {
+                  type: 'hmm'
+                }
+              }
             }
           }
         }
